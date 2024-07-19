3c7079e2bda9361b3359ed7384f39307027a3dd7
Bayron Vazquez <bayronnjonathan@gmail.com>
2024-07-19 08:52:28 -0500

    example of functions and dynamic content
    In this commit we use the for loop created previously
    in the list of <li> elements to give it a behavior
    every time each element is clicked.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,43 @@
-function ListGroup() {
-	const paises = ["Mexico", "Estados Unidos", "Colombia", "Peru"];
+import { string } from "prop-types";
 
-	return (
-		<div className="col-12">
-			<ul className="list-group">
-				{paises.map(pais => (
-					<li key={pais}>{pais}</li>
-				))}
-			</ul>
-		</div>
-	);
+function elementClicked(pais: string)
+{
+	console.log('clicked', pais);
+}
+
+function ListGroup()
+{
+	const paises = [
+		"Mexico",
+		"Estados Unidos",
+		"Colombia", 
+		"Peru"
+	];
+
+	if (paises.length == 0)
+	{
+		return <h1>NO items Found</h1>;
+	}
+	else
+	{
+		return (
+			<div className="col-12">
+				<ul className="list-group">
+					{
+						paises.map( (pais, index) => (
+							<li
+								key={pais}
+								onClick={() => elementClicked(pais)}
+								className="list-group-item"
+							>
+								{pais}
+							</li>
+						))
+					}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default ListGroup;
